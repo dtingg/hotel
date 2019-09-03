20243152f00c9bfb8f23c6178bea822e7a7999cc
Dianna Tingg <dtingg@gmail.com>
2019-09-03 16:11:32 -0700

    added reservation.total_cost method and test

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -9,5 +9,11 @@ module Hotel
       @check_in = Date.parse(check_in)
       @check_out = Date.parse(check_out)
     end
+    
+    def total_cost
+      nights = check_out - check_in
+      
+      return nights * room.nightly_cost
+    end
   end
 end
--- test/reservation_test.rb
+++ test/reservation_test.rb
@@ -36,4 +36,13 @@ describe "Reservation class" do
       expect(reservation.check_out).must_equal Date.parse("August 5, 2019")
     end
   end  
+  
+  describe "Total cost method" do
+    it "Returns the cost for a reservation" do
+      room = Hotel::Room.new(5)
+      reservation = Hotel::Reservation.new(1, room, "August 1, 2019", "August 5, 2019")
+      
+      expect(reservation.total_cost).must_equal 800
+    end
+  end
 end
