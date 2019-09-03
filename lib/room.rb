7d6d6db28b3327ddd9de73703b0a07a72d0b3a41
Dianna Tingg <dtingg@gmail.com>
2019-09-03 13:37:01 -0700

    added room.initialize and tests

--- lib/room.rb
+++ lib/room.rb
@@ -1,9 +1,17 @@
 module Hotel
-  class Room
-    attr_reader :id
+  class Room    
+    NUMBER_OF_ROOMS = 20
+    
+    attr_reader :id, :nightly_cost, :reservations
     
     def initialize(id)
+      if id <= 0 || id > 20
+        raise ArgumentError.new("The hotel only has rooms 1 - 20.")
+      end
+      
       @id = id
+      @nightly_cost = 200
+      @reservations = []
     end
   end
 end
--- test/room_test.rb
+++ test/room_test.rb
@@ -7,5 +7,34 @@ describe "Room class" do
       
       expect(room).must_be_kind_of Hotel::Room
     end    
+    
+    it "Keeps track of id" do
+      room = Hotel::Room.new(1)
+      
+      expect(room).must_respond_to :id
+    end
+    
+    it "Can have an id between 1 and 20" do
+      expect(Hotel::Room.new(1).id).must_equal 1
+      expect(Hotel::Room.new(20).id).must_equal 20
+    end
+    
+    it "Raises an ArgumentError if id is outside of 1 - 20" do
+      expect{Hotel::Room.new(0)}.must_raise ArgumentError
+      expect{Hotel::Room.new(21)}.must_raise ArgumentError
+    end
+    
+    it "Has a default nightly cost" do
+      room = Hotel::Room.new(1)
+      
+      expect(room.nightly_cost).must_equal 200
+    end
+    
+    it "Has a default empty array of reservations" do
+      room = Hotel::Room.new(1)
+      
+      expect(room.reservations).must_be_kind_of Array
+      expect(room.reservations).must_equal []
+    end
   end
 end
